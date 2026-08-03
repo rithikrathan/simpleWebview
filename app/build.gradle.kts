17a9f72ade5c9d054cb35df786f7b8cbc0c3042a
Rathan <rithikrathan.c@gmail.com>
2026-08-03 14:44:07 +0530

    Fix typo in cleartext traffic attribute, bump to 2.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.rithikrathan.simplewebview"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "2.1.1"
+        versionCode = 6
+        versionName = "2.1.2"
     }
 
     signingConfigs {
