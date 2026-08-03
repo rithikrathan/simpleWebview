2c0bef563df664a384d357602c2f8e4ba8665f4e
Rathan <rithikrathan.c@gmail.com>
2026-08-03 14:38:25 +0530

    Allow cleartext traffic for http webpages, bump to 2.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.rithikrathan.simplewebview"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "2.1.0"
+        versionCode = 5
+        versionName = "2.1.1"
     }
 
     signingConfigs {
